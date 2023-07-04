7aedb485bb2a21bb7733f6f702a900bee1ce615f
YeLinAung <40236753+b14cknc0d3@users.noreply.github.com>
2023-07-04 13:24:06 +0630

    refreshToken method added

--- lib/src/helper/constant.dart
+++ lib/src/helper/constant.dart
@@ -1,5 +1,6 @@
+// ignore_for_file: constant_identifier_names
+
 class AppConstant {
-  // ignore: constant_identifier_names
   static const String BASE_URL = 'https://api.ihealthlabs.com:8443/OpenApiV2';
 
   static const String BASE_URI = "api.ihealthlabs.com";
